84467d485e535f24f707ce2c2ccf68c150f281fd
Alikhan <tuxubaev17@gmail.com>
2021-11-09 12:43:03 +0500

    create firstViewController

--- TestGitFlowProject/FirstViewController.swift
+++ TestGitFlowProject/FirstViewController.swift
@@ -5,14 +5,11 @@
 //  Created by Alikhan Tuxubayev on 09.11.2021.
 //
 
-import Foundation
 import UIKit
-
 final class FirstViewController: UIViewController {
     override func viewDidLoad() {
-         super.viewDidLoad()
-        
         let subView = UIView()
         view.addSubview(subView)
+        view.backgroundColor = .systemYellow
     }
 }
