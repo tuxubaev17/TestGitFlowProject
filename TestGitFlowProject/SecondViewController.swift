7274337922a591a60e5a530c41239c6796a8a6f7
Alikhan <tuxubaev17@gmail.com>
2021-11-09 14:14:26 +0500

    Create SecondViewController and change view -> subview

---
+++ TestGitFlowProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestGitFlowProject
+//
+//  Created by Alikhan Tuxubayev on 09.11.2021.
+//
+
+import Foundation
+import UIKit
+
+final class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subView = UIView()
+        subView.backgroundColor = .red
+        subView.frame = view.bounds
+        view.addSubview(subView)
+    }
+}
