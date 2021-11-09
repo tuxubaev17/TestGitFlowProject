82987d2879321baf53227fc8dab9d8fd2b7baf87
Alikhan <tuxubaev17@gmail.com>
2021-11-09 13:57:52 +0500

    Create firstViewController and add subview

---
+++ TestGitFlowProject/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  TestGitFlowProject
+//
+//  Created by Alikhan Tuxubayev on 09.11.2021.
+//
+
+import Foundation
+import UIKit
+
+final class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+        let subView = UIView()
+        view.addSubview(subView)
+    }
+}
